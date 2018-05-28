63a4f8da3ffa18dc21aff2d0a04da2827b842d61
Daniel Lertsirikarn <mightlert@hotmail.com>
2018-05-28 01:02:15 -0400

    change main

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from . import createppt
+
+
+from flask import Flask, request, send_from_directory, send_file 
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__)) 
+
+savedirectory = os.path.join(APP_ROOT, 'finishedppt')
+savefile = os.path.join(savedirectory, 'WorshipSongs.pptx')
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+    APP_ROOT = os.path.dirname(os.path.abspath(__file__)) 
+    lyricfile = os.path.join(APP_ROOT, 'songdata/amazing grace.xml')
+    createppt.getsongdata(lyricfile,1,1,1)
+    try:
+        return send_file(savefile,as_attachment=True)
+    except Exception as e:
+        return str(e)
+    
 
 if __name__ == '__main__':
   app.run()
