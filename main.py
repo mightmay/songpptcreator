87aeab9f19bc052ee00b1b1b01425ef047f7a948
Daniel Lertsirikarn <mightlert@hotmail.com>
2018-05-28 04:26:00 -0400

    asf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append("pymodules")
-from .createppt import * 
+#from .createppt import * 
 from flask import Flask, request, send_from_directory, send_file 
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__)) 
