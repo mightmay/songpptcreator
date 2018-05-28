80afd5fb3187b1fb0e40344f76d8b71304148a2f
Daniel Lertsirikarn <mightlert@hotmail.com>
2018-05-28 01:10:10 -0400

    ss

--- main.py
+++ main.py
@@ -9,11 +9,8 @@ from flask import Flask, request, send_from_directory, send_file
 @app.route('/')
 def hello_world():
 
-    #createppt.getsongdata(lyricfile,1,1,1)
-    try:
-        return "test"
-    except Exception as e:
-        return str(e)
+    return 'Hello, World!'
+
     
 
 if __name__ == '__main__':
