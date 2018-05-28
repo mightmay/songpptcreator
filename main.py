d18f413b27867429b07a722302a8dc9e831ac931
Daniel Lertsirikarn <mightlert@hotmail.com>
2018-05-28 01:05:58 -0400

    test3

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ from flask import Flask, request, send_from_directory, send_file
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__)) 
 
-savedirectory = os.path.join(APP_ROOT, 'finishedppt')
-savefile = os.path.join(savedirectory, 'WorshipSongs.pptx')
+
 
 @app.route('/')
 def hello_world():
-    APP_ROOT = os.path.dirname(os.path.abspath(__file__)) 
-    lyricfile = os.path.join(APP_ROOT, 'songdata/amazing grace.xml')
+
     #createppt.getsongdata(lyricfile,1,1,1)
     try:
         return "test"
