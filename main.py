af86fd93f0e419aa9780b251d601cfb2d6cdc319
Daniel Lertsirikarn <mightlert@hotmail.com>
2018-05-28 01:11:26 -0400

    ss

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-
-
-
-
-from flask import Flask, request, send_from_directory, send_file 
-
-
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-
-    return 'Hello, World!'
-
-    
+  return 'Hello, World!'
 
 if __name__ == '__main__':
   app.run()
