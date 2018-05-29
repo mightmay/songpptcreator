da8bd6284e3cd5139b8213c64f85c01099b8f4c1
Daniel Lertsirikarn <mightlert@hotmail.com>
2018-05-28 22:31:07 -0400

    afsfs

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import traceback
 app = Flask(__name__)
 #import createppt
 
-#    try:
-#        import lxml
-#    except Exception:
-#        tb=str(traceback.format_exc())
-#        return(tb)
+
+
 @app.route('/')
 def hello_world():
+    try:
+        import lxml
+    except Exception:
+        tb=str(traceback.format_exc())
+        return(tb)
     returnstring="start..."
     returnstring=returnstring+"import..."
     returnstring=returnstring+"createppt.imported..."
