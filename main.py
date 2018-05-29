0f2b72c879c35cf1e4279cde817cea6c208ba7aa
Daniel Lertsirikarn <mightlert@hotmail.com>
2018-05-28 22:08:00 -0400

    csa

--- main.py
+++ main.py
@@ -12,14 +12,11 @@ def hello_world():
     returnstring=returnstring+"import..."
     import createppt
     returnstring=returnstring+"createppt.imported..."
-
     try:
-        #import lxml
-    except Exception as e:
-        tb = e.__traceback__
-        tb = traceback.format_exc()
-        returnstring=returnstring+ tb
-        return (returnstring)
+        import lxml
+    except Exception:
+        traceback=str(traceback.format_exc())
+        returnstring=returnstring+traceback
     return(returnstring)
 
 if __name__ == '__main__':
