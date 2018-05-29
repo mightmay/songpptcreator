0925ac3ed592830a6a29884febc22f55e9992764
Daniel Lertsirikarn <mightlert@hotmail.com>
2018-05-28 21:27:41 -0400

    asfas

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 
-returnstring="<!DOCTYPE html><html><body>"
-returnstring = returnstring+'importing<br>'
+returnstring="test"
+returnstring = returnstring+'importing..'
 from flask import Flask
 
-returnstring = returnstring + 'import done<br>'
+returnstring = returnstring + 'import done..'
 
 app = Flask(__name__)
 returnstring = ''
 @app.route('/')
 def hello_world():
 
-    returnstring= returnstring+"</body></html>"
     return(returnstring)
 
 if __name__ == '__main__':
