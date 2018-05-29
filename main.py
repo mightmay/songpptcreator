8838d783c83752dfe2a0e19c3d3f00b1fe0aee5a
Daniel Lertsirikarn <mightlert@hotmail.com>
2018-05-28 21:35:59 -0400

    savs

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 
 
 from flask import Flask
-returnstring="start..."
-returnstring=returnstring+"import..."
+
 
 
 app = Flask(__name__)
 returnstring = ''
 @app.route('/')
 def hello_world():
+    returnstring="start..."
+    returnstring=returnstring+"import..."
+    import createppt
+    returnstring=returnstring+"createppt.imported..."
 
+    try:
+        import lxml
+    except Exception as e:
+        tb = e.__traceback__
+        tb = traceback.format_exc()
+        return tb
     return(returnstring)
 
 if __name__ == '__main__':
